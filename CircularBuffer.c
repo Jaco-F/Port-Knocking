d95e5f60f8cbc89f078f2ba1917437a893e8e7c4
Jacopo Fiorenza <jacopo.f91@gmail.com>
2013-05-28 11:04:45 +0200

    Circularbuffer e intercettatore pacchetti TCP

---
+++ CircularBuffer.c
@@ -0,0 +1,111 @@
+#include <linux/module.h>
+#include <linux/slab.h>
+#include "CircularBuffer.h"
+
+MODULE_LICENSE("GPL");
+ 
+MODULE_DESCRIPTION("circular-buffer");
+ 
+MODULE_AUTHOR("4Mosfet");
+ 
+/* Opaque buffer element type.  This would be defined by the application. */
+
+
+/* Circular buffer object */
+static int PortSequence[8];
+ 
+void cbInit(CircularBuffer *cb, int size) {
+    cb->size  = size + 1; /* include empty elem */
+    cb->start = 0;
+    cb->end   = 0;
+    cb->elems = kmalloc(size*sizeof(ElemType), GFP_KERNEL);
+    PortSequence[0]=80;
+    PortSequence[1]=81;
+    PortSequence[2]=82;
+    PortSequence[3]=83;
+    PortSequence[4]=84;
+    PortSequence[5]=85;
+    PortSequence[6]=86;
+    PortSequence[7]=87;
+}
+void delete(int posizione,CircularBuffer *cb);
+
+void cbFree(CircularBuffer *cb) {
+    kfree(cb->elems); /* OK if null */ }
+ 
+int cbIsFull(CircularBuffer *cb) {
+    return (cb->end + 1) % cb->size == cb->start; }
+ 
+int cbIsEmpty(CircularBuffer *cb) {
+    return cb->end == cb->start; }
+ 
+/* Write an element, overwriting oldest element if buffer is full. App can
+   choose to avoid the overwrite by checking cbIsFull(). */
+void cbWrite(CircularBuffer *cb, ElemType *elem) {
+    //printk(KERN_INFO "struct IN packet info: src ip: %u, src port: %u; dest ip: %u, dest port: %u\n", elem->src_ip, elem->src_port, elem->dest_ip, elem->dest_port);
+    int i=0;
+    int j=0;
+    printk(KERN_INFO "struct IN packet info: src ip: %u, dest port: %u\n", elem->src_ip, elem->dest_port);
+    printk(KERN_INFO "end : %d" ,cb->end);
+    if(cb->end == 0) {
+      printk(KERN_INFO "primo porta : %u , seq : %u\n", elem->dest_port,PortSequence[0]);
+      if (elem->dest_port == PortSequence[0])	{		//Se è la prima allora sovrascrivo altrimenti cancello
+	printk(KERN_INFO "primo inserito");
+	cb->elems[cb->end] = *elem;				//Aggiungo un elemento in coda al vettore
+	cb->end = (cb->end + 1) % cb->size;
+	printk(KERN_INFO "end : %d" ,cb->end);
+	if (cb->end == cb->start)
+	  cb->start = (cb->start + 1) % cb->size;
+      }	 
+    }
+    else {
+      for(i=0; i <=cb->end ; i++) {          				//Scorre il buffer
+	if(elem->src_ip == cb->elems[i].src_ip) {			//Controlla se ha gia ricevuto un pacchetto con lo stesso IP
+	  printk(KERN_INFO "gia' ricevuto da ip uguale");
+	  for(j=0;j < 8;j++) {						//Scorre il vettore di PortSequence
+	    if((cb->elems[i].dest_port == PortSequence[j])&&(PortSequence[j+1] == elem->dest_port)) {//Controlla a che porta è arrivato in precedenza e se la porta corrisponde a quella che mi aspetto sostituisco
+	      printk(KERN_INFO "vecchia destport %u" , cb->elems[i].dest_port);
+	      cb->elems[i] = *elem;
+	      printk(KERN_INFO "avanziamo di porta!!");
+	      printk(KERN_INFO "end : %d" ,cb->end);
+	      if(j+1 == 7) {
+		printk(KERN_INFO "RICONOSCIUTA");
+		delete(i,cb);
+	      }
+	    }
+	    else { 
+	      if((cb->elems[i].dest_port == PortSequence[j])&&(PortSequence[j+1] != elem->dest_port)) {						//Se la porta non corrisponde a nessuna di quelle del vettore
+		if (elem->dest_port == PortSequence[0])		//Se è la prima allora sovrascrivo altrimenti cancello
+		  cb->elems[i] = *elem;			
+		else {
+		  printk(KERN_INFO "eliminato");
+		  delete(i,cb);
+		}
+	      }
+	    }
+	    printk(KERN_INFO "FUORI!!!");
+	    break;
+	  }
+	}
+	else {							//Se non ho ricevuto un pacchetto con lo stesso IP
+	  cb->elems[cb->end] = *elem;				//Aggiungo un elemento in coda al vettore
+	  cb->end = (cb->end + 1) % cb->size;
+	  if (cb->end == cb->start)
+	    cb->start = (cb->start + 1) % cb->size; /* full, overwrite */
+	}
+      }
+    }
+}
+ 
+/* Read oldest element. App must ensure !cbIsEmpty() first. */
+void cbRead(CircularBuffer *cb, ElemType *elem) {
+    *elem = cb->elems[cb->start];
+    cb->start = (cb->start + 1) % cb->size;
+}
+void delete(int posizione,CircularBuffer *cb) {
+    int i;
+    for (i = posizione; i<=cb->end; i++) {
+      cb->elems[i]=cb->elems[i+1];
+    }
+    cb->end = (cb->end-1) % cb->size;
+}
---
+++ provaTcp.c
@@ -0,0 +1,149 @@
+#include <linux/module.h>
+ 
+#include <linux/kernel.h>
+ 
+#include <linux/proc_fs.h>
+ 
+#include <linux/list.h>
+ 
+#include <asm/uaccess.h>
+ 
+#include <linux/udp.h>
+ 
+#include <linux/tcp.h>
+ 
+#include <linux/skbuff.h>
+ 
+#include <linux/ip.h>
+
+#include <net/ip.h>
+ 
+#include <linux/netfilter.h>
+ 
+#include <linux/netfilter_ipv4.h>
+
+#include "CircularBuffer.h"
+
+
+MODULE_LICENSE("GPL");
+ 
+MODULE_DESCRIPTION("linux-simple-firewall");
+ 
+MODULE_AUTHOR("4Mosfet");
+
+//the structure used to register the function
+ 
+static struct nf_hook_ops nfho;
+
+static CircularBuffer cb;
+
+static CircularBuffer *bufferPointer=NULL;
+
+static ElemType e;
+ 
+//the hook function itself: registered for filtering incoming packets
+ 
+unsigned int hook_func_in(unsigned int hooknum, struct sk_buff *skb, 
+ 
+        const struct net_device *in, const struct net_device *out,
+ 
+        int (*okfn)(struct sk_buff *)) {
+ 
+   /*get src address, src netmask, src port, dest ip, dest netmask, dest port, protocol*/
+ 
+   struct iphdr *ip_header = (struct iphdr *)skb_network_header(skb);
+ 
+   struct tcphdr *tcp_header;
+   
+   
+
+ 
+   /**get src and dest ip addresses**/
+ 
+   
+   unsigned int src_ip= ip_header->saddr;
+   unsigned int dest_ip= ip_header->daddr;
+   /*char src_ip[16];
+   snprintf(src_ip, 16, "%pI4", &ip_header->saddr);
+
+   char dest_ip[16];
+   snprintf(dest_ip, 16, "%pI4", &ip_header->daddr);*/
+ 
+   
+ 
+   unsigned int src_port = 0;
+ 
+   unsigned int dest_port = 0;
+ 
+   /***get src and dest port number***/
+ 
+
+ 
+    //	TCP
+   if (ip_header->protocol == 6) {
+       
+       
+ 
+       tcp_header = (struct tcphdr *)(skb_network_header(skb) + ip_hdrlen(skb));
+       
+       //if(tcp_header->syn == 1) {
+	  if (bufferPointer == NULL) { 
+	    printk(KERN_INFO "inizializzo");
+	    cbInit(&cb,10);
+	    bufferPointer = &cb;
+          }
+ 
+       src_port = (unsigned int)ntohs(tcp_header->source);
+ 
+       dest_port = (unsigned int)ntohs(tcp_header->dest);
+       
+       e.src_ip = src_ip;
+       e.src_port = src_port;
+       e.dest_ip = dest_ip;
+       e.dest_port = dest_port; 
+       
+       cbWrite(bufferPointer,&e);
+	 
+       //printk(KERN_INFO "IN packet info: src ip: %u, src port: %u; dest ip: %u, dest port: %u\n", e.src_ip, e.src_port, e.dest_ip, e.dest_port);
+      //}
+ 
+   }
+				
+   
+    
+   
+   return 0;
+
+ }
+ 
+ 
+/* Initialization routine */
+ 
+int init_module() {
+    printk(KERN_INFO "initialize kernel module\n");
+ 
+    /* Fill in the hook structure for incoming packet hook*/
+ 
+    nfho.hook = hook_func_in;
+ 
+    nfho.hooknum = NF_INET_LOCAL_IN;
+ 
+    nfho.pf = PF_INET;
+ 
+    nfho.priority = NF_IP_PRI_FIRST;
+ 
+    nf_register_hook(&nfho);         // Register the hook
+  
+    //cbInit(&cb,10);
+ 
+    return 0;
+ 
+}
+
+void cleanup_module() {
+
+    nf_unregister_hook(&nfho);
+ 
+    printk(KERN_INFO "kernel module unloaded.\n");
+ 
+}
